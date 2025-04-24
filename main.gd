67c7a3b496dd44ed8abe824479e413a6913349fd
bzaPetey <peter.laliberte@burgzergarcade.net>
2025-04-24 06:28:31 -0400

    init commit

---
+++ main.gd
@@ -0,0 +1,30 @@
+class_name Main extends Node3D
+
+@export var port:int = 10024
+@export var player_prefab:PackedScene
+
+@onready var menu: CanvasLayer = $Menu
+
+
+var peer:ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+
+
+func _on_btn_start_pressed() -> void:
+	menu.hide()
+	peer.create_server(port)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(add_player)
+	add_player(multiplayer.get_unique_id())
+
+
+func _on_btn_join_pressed() -> void:
+	menu.hide()
+	peer.create_client("127.0.0.1", port)
+	multiplayer.multiplayer_peer = peer
+
+
+func add_player(id:int) -> void:
+	var player = player_prefab.instantiate() as Player
+	player.name = str(id)
+	add_child(player)
+	
