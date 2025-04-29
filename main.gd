b608ebf1647d20114f0a91a24583a8ddf89dd15c
bzaPetey <burgzergarcade@gmail.com>
2025-04-29 00:21:11 -0400

    monday class update

--- main.gd
+++ main.gd
@@ -3,13 +3,13 @@ class_name Main extends Node3D
 @export var port:int = 10024
 @export var player_prefab:PackedScene
 
-@onready var menu: CanvasLayer = $Menu
-
+@onready var menu: CanvasLayer = $CanvasLayer
 
 var peer:ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 
 
-func _on_btn_start_pressed() -> void:
+
+func _on_btn_server_pressed() -> void:
 	menu.hide()
 	peer.create_server(port)
 	multiplayer.multiplayer_peer = peer
@@ -17,10 +17,12 @@ func _on_btn_start_pressed() -> void:
 	add_player(multiplayer.get_unique_id())
 
 
-func _on_btn_join_pressed() -> void:
+
+func _on_btn_client_pressed() -> void:
 	menu.hide()
 	peer.create_client("127.0.0.1", port)
 	multiplayer.multiplayer_peer = peer
+
 
 
 func add_player(id:int) -> void:
--- player.gd
+++ player.gd
@@ -1,30 +1,47 @@
 class_name Player extends CharacterBody3D
 
+@export var mouse_sensitivity:float = 0.002
+
 @onready var cam: Camera3D = $Camera3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
+const SPEED:float = 5.0
+const JUMP_VELOCITY:float = 4.5
 
 func _enter_tree() -> void:
-	set_multiplayer_authority(str(name).to_int())
-	
+	set_multiplayer_authority(name.to_int())
+
+
 
 func _ready() -> void:
 	if not is_multiplayer_authority():
 		return
-
+	
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	cam.current = true
 
 
-func enter_tree() -> void:
-	set_multiplayer_authority(name.to_int())
+func _unhandled_input(event: InputEvent) -> void:
+	if not is_multiplayer_authority():
+		return
 	
+	# give us back the cursor when esc is pressed
+	if event is InputEventKey and event.pressed and event.keycode == KEY_ESCAPE:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+
+	if event is InputEventMouseMotion:
+		# rotate self left and right
+		rotate_y(-event.relative.x * mouse_sensitivity)
+
+		# rotate camera up and down
+		cam.rotate_x(-event.relative.y * mouse_sensitivity)
+		cam.rotation.x = clampf(cam.rotation.x, -deg_to_rad(90), deg_to_rad(90))
+		
 
 func _physics_process(delta: float) -> void:
 	if not is_multiplayer_authority():
 		return
-
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
